f6eb55d2f7f57b5d01bb9f9baee81ed0011114bf
pauel <pau.lacazedieu@gmail.com>
2026-08-04 15:12:03 +0200

    Fix LinkMe so it actually works again.

--- db-manager/src/main/kotlin/Discord/LinkMe.kt
+++ db-manager/src/main/kotlin/Discord/LinkMe.kt
@@ -12,26 +12,18 @@ import dev.kordex.core.extensions.ephemeralSlashCommand
 import dev.kordex.i18n.Key
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import java.util.Locale
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
 
-class LinkMeExtension(val db: DB, val linkedRole: Snowflake, val linkedGuild: Snowflake): Extension() {
+class LinkMeExtension(val db: DB, val linkedRole: Snowflake, val linkedGuild: Snowflake) : Extension() {
     override val name: String = "linkMe"
     val codesToSteamIDs = mutableMapOf<Int, ULong>()
 
-    inner class LinkMeArguments: Arguments() {
+    class LinkMeArguments : Arguments() {
         val code by int {
             name = Key("code")
             description = Key("code you got in the game")
-        }
-
-        override fun validate(locale: Locale) {
-            super.validate(locale)
-            if (!codesToSteamIDs.containsKey(code)){
-                error("the code $code you have given was not registered. Use /linkme in-game first!")
-            }
         }
     }
 
@@ -48,14 +40,16 @@ class LinkMeExtension(val db: DB, val linkedRole: Snowflake, val linkedGuild: Sn
                 }
                 if (db.isUserInDb(user.id.toString())) {
                     fail(Key("Your discord was already linked. You cannot do it again."))
+                    return@check
                 }
+                pass()
             }
 
             action {
                 val steamID = codesToSteamIDs[arguments.code]
                 if (steamID == null) {
                     respond {
-                        content = "The code you entered is invalid."
+                        content = "the code ${arguments.code} you have given was not registered. Use /linkme in-game first!"
                     }
                     return@action
                 }
@@ -69,28 +63,28 @@ class LinkMeExtension(val db: DB, val linkedRole: Snowflake, val linkedGuild: Sn
         }
     }
 
-    fun newLink(link: LinkUser){
+    fun newLink(link: LinkUser) {
         codesToSteamIDs[link.oneTimeCode] = link.senderSteamID.toULong()
         kord.launch {
             delay(10.minutes)
-            if (codesToSteamIDs.keys.contains(link.oneTimeCode)){
+            if (codesToSteamIDs.keys.contains(link.oneTimeCode)) {
                 codesToSteamIDs.remove(link.oneTimeCode)
             }
         }
     }
 
-    suspend fun addLinkedRole(userId: Snowflake){
+    suspend fun addLinkedRole(userId: Snowflake) {
         try {
             val guild = kord.getGuild(linkedGuild)
             val member = guild.getMember(userId)
             member.addRole(linkedRole)
-        }catch (e: EntityNotFoundException){
+        } catch (e: EntityNotFoundException) {
             println("Could not add linked role to user ${userId.value}: ${e.message}")
+        } catch (_: Exception) {
         }
-        catch (_: Exception) { }
     }
 
-    suspend fun initLinkedRoles(){
+    suspend fun initLinkedRoles() {
         db.getLinkedUsers().chunked(5).forEach { batch ->
             batch.forEach { user -> addLinkedRole(Snowflake(user)) }
             delay(1.seconds)
